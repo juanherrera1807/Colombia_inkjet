29727ea85f65e5741942a127f2497e0b063b2c64
Fausto Guzman <dg_faco@hotmail.com>
2023-09-27 15:21:39 -0500

    arreglos pag inicio-Fausto

--- src/App.vue
+++ src/App.vue
@@ -17,61 +17,23 @@
        </v-app-bar>
  
        <v-main>
-         <div>
-           <h1>Contrato de Alquiler de Impresoras</h1>
- 
-           <p>Este contrato de alquiler de impresoras ("Contrato") se celebra entre [Nombre de la Empresa Arrendadora], con domicilio en [Dirección], en adelante denominada "Arrendadora", y [Nombre del Cliente], con domicilio en [Dirección del Cliente], en adelante denominado "Cliente". Ambas partes acuerdan los siguientes términos y condiciones:</p>
- 
-           <h2>1. Objeto del Contrato</h2>
-           <p>La Arrendadora acuerda alquilar al Cliente el siguiente equipo de impresión:</p>
- 
-           <ul>
-             <li>Modelo de impresora: [Nombre o modelo de la impresora]</li>
-             <li>Cantidad: [Número de impresoras alquiladas]</li>
-           </ul>
- 
-           <h2>2. Duración del Alquiler</h2>
-           <p>El período de alquiler comenzará el [Fecha de inicio del alquiler] y terminará el [Fecha de finalización del alquiler]. El Cliente se compromete a devolver el equipo de impresión en buenas condiciones al finalizar el período de alquiler.</p>
- 
-           <h2>3. Pago y Tarifas</h2>
-           <p>El Cliente acuerda pagar a la Arrendadora la tarifa de alquiler mensual de [Monto del alquiler] antes del [Día de pago] de cada mes. El primer pago se realizará el [Fecha del primer pago].</p>
- 
-           <h2>4. Mantenimiento y Reparaciones</h2>
-           <p>La Arrendadora será responsable de proporcionar el mantenimiento y las reparaciones necesarias para mantener el equipo de impresión en funcionamiento adecuado durante el período de alquiler. El Cliente debe informar cualquier problema o avería a la Arrendadora de inmediato.</p>
- 
-           <h2>5. Obligaciones del Cliente</h2>
-           <p>El Cliente se compromete a:</p>
- 
-           <ul>
-             <li>Utilizar el equipo de impresión de manera adecuada y solo para fines comerciales legítimos.</li>
-             <li>No realizar modificaciones o alteraciones en el equipo de impresión sin el consentimiento de la Arrendadora.</li>
-             <li>Mantener el equipo en un entorno adecuado y seguro.</li>
-           </ul>
- 
-           <h2>6. Terminación del Contrato</h2>
-           <p>Este Contrato puede ser terminado por cualquiera de las partes con previo aviso por escrito con al menos [Número de días de notificación] días de antelación.</p>
- 
-           <h2>7. Devolución del Equipo</h2>
-           <p>Al finalizar el período de alquiler o en caso de terminación anticipada, el Cliente debe devolver el equipo de impresión a la Arrendadora en condiciones de funcionamiento adecuadas, salvo el desgaste normal por uso.</p>
- 
-           <h2>8. Ley Aplicable</h2>
-           <p>Este Contrato se rige por las leyes del [País o Estado] y cualquier disputa relacionada con el mismo estará sujeta a la jurisdicción exclusiva de los tribunales del [País o Estado].</p>
- 
-           <h2>9. Firma</h2>
-           <p>Las partes acuerdan este Contrato mediante sus firmas electrónicas o manuscritas a continuación:</p>
- 
-           <div class="firmas">
-             <div class="firma-cliente">
-               <p>Firma del Cliente: ____________________________</p>
-               <p>Fecha: ________________</p>
-             </div>
- 
-             <div class="firma-arrendadora">
-               <p>Firma de la Arrendadora: ________________________</p>
-               <p>Fecha: ________________</p>
-             </div>
-           </div>
-         </div>
+        <div class="parent">
+          <div class="div1"> </div>
+          <div class="div2"> 
+            <v-img src="@/assets/prueba.png"></v-img>
+          </div>
+          <div class="div3"> 
+
+          </div>
+          <div class="div4">  
+            <h1 class="tituloUno">Simplificamos  la  Impresión  para  tu  Negocio.</h1>
+            <p>Maximiza tu productividad con nuestras impresoras de alta calidad y olvida las preocupaciones 
+              gracias a nuestro servicio integral de mantenimiento de cartuchos. En Colombia Inkjet, 
+              somos tu socio estratégico en Santander para todas tus necesidades de impresión y tinta. 
+              Con nosotros, puedes concentrarte en lo que mejor haces, mientras nosotros nos ocupamos 
+              de mantener tus impresoras funcionando sin problemas y tus documentos impresos con la máxima calidad.</p>
+          </div>
+        </div>  
  <!-- Mostrar el componente de inicio de sesión si no está autenticado -->
         
          
@@ -121,4 +83,21 @@
    },
  };
  </script>
- 
+<style>
+.parent {
+display: grid;
+grid-template-columns: 0.5fr repeat(10, 1fr) 0.5fr;
+grid-template-rows: 0fr repeat(5, 0.9fr) 0.2fr;
+grid-column-gap: 5px;
+grid-row-gap: 5px;
+}
+
+.div1 { grid-area: 1 / 1 / 8 / 13; }
+.div2 { grid-area: 2 / 6 / 7 / 13; }
+.div3 { grid-area: 6 / 2 / 7 / 6; }
+.div4 { grid-area: 2 / 2 / 6 / 6; } 
+.tituloUno{color: rgb(33, 150, 253);font-size: 35pt;text-align: start; padding-bottom: 4%;font-family: Spinnaker;}
+
+p{text-align:start;padding-top: 2%;font-size: 12pt;font-family: Spinnaker;}
+
+</style>
